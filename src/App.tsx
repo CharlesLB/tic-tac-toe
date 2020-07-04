cbf155b6644b5f6830b028346a57805661c2fd04
Charles Lelis Braga <59098599+CharlesLB@users.noreply.github.com>
2020-07-04 12:44:07 -0300

    Modularization  :scissors:

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import React from 'react';
+import React from "react";
+import Game from "./components/Game";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <Game />;
 }
 
 export default App;
